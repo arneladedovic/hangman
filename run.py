5c60b630a65afb904981c4d003c7df88c0c49a66
arneladedovic <arneladedovic@hotmail.com>
2022-07-23 20:16:53 +0000

    Add function that gets a random word from the wordlist in words.py

--- run.py
+++ run.py
@@ -14,11 +14,18 @@ def logo():
     _|    _|  _|    _|  _|      _|    _|_|_|  _|      _|  _|    _|  _|      _| 
     """)
 
+def get_word():
+    """
+    Function that gets a random word from words.py.
+    """
+    word = random.choice(word_list)
+    return word.upper()
 
 def main():
     """
     Main function that runs the game
     """
     logo()
+    word = get_word()
 
 main()
